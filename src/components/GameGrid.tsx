436db109e1f81ac8126468d30f0a8b99bd8196fa
Marcus Brose <contact@marcusbrose.com>
2023-05-30 15:44:13 +0200

    refactor duplicate styles

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,6 +2,7 @@ import { SimpleGrid, Text } from '@chakra-ui/react'
 import useGames from '../hooks/useGames'
 import GameCard from './GameCard'
 import GameCardSkeleton from './GameCardSkeleton'
+import GameCardContainer from './GameCardContainer'
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGames()
@@ -11,8 +12,14 @@ const GameGrid = () => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding={10} spacing={10}>
-        {isLoading && skeletons.map((_, index) => <GameCardSkeleton key={index} />)}
-        {games.map((game) => <GameCard key={game.id} game={game} />)}
+        {isLoading && skeletons.map((_, index) =>
+          <GameCardContainer>
+            <GameCardSkeleton key={index} />
+          </GameCardContainer>)}
+        {games.map((game) =>
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>)}
       </SimpleGrid>
     </>
   )
